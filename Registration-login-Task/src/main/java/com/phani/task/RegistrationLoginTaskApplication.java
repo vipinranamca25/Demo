9640ebae1d2bce4af47189669227ea0053065251
vipinranamca25 <61753334+vipinranamca25@users.noreply.github.com>
2022-05-05 07:45:21 -0400

    demo
    demo1 src code

---
+++ Registration-login-Task/src/main/java/com/phani/task/RegistrationLoginTaskApplication.java
@@ -0,0 +1,13 @@
+package com.phani.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationLoginTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationLoginTaskApplication.class, args);
+	}
+
+}
---
+++ Registration-login-Task/src/test/java/com/phani/task/RegistrationLoginTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phani.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationLoginTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
